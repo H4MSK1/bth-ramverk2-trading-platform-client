b7ec5a49588bb9153e4877ca3cc9b4b26d6ea037
H4MSK1 <alburhan97@gmail.com>
2019-10-25 23:22:48 +0200

    update document.title according to current route

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -2,26 +2,34 @@ import React from 'react';
 import Navbar from 'navigation/Navbar';
 import { DefaultContainer } from './DefaultContainer';
 
-export const AppLayout = ({ children }) => (
-  <div className="app">
-    <header className="app-navbar-wrapper">
-      <Navbar />
-    </header>
+export const AppLayout = ({ children, title }) => {
+  React.useEffect(() => {
+    if (title) {
+      document.title = `${process.env.REACT_APP_SITE_TITLE} - ${title}`;
+    }
+  }, [title]);
 
-    <main className="app-main-content">{children}</main>
+  return (
+    <div className="app">
+      <header>
+        <Navbar />
+      </header>
 
-    <footer className="app-footer text-light">
-      <DefaultContainer>
-        This site was created with React & Reactstrap.{' '}
-        <a
-          href={process.env.REACT_APP_GITHUB_REPOSITORY}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-light"
-        >
-          {process.env.REACT_APP_GITHUB_REPOSITORY}
-        </a>
-      </DefaultContainer>
-    </footer>
-  </div>
-);
+      <main className="app-main-content">{children}</main>
+
+      <footer className="app-footer text-light">
+        <DefaultContainer>
+          This site was created with React & Reactstrap.{' '}
+          <a
+            href={process.env.REACT_APP_GITHUB_REPOSITORY}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-light"
+          >
+            {process.env.REACT_APP_GITHUB_REPOSITORY}
+          </a>
+        </DefaultContainer>
+      </footer>
+    </div>
+  );
+};
