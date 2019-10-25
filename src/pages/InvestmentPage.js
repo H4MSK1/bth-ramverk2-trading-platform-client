1014f010eb21b1e9639cf347973eecfa8ef1a190
H4MSK1 <alburhan97@gmail.com>
2019-10-25 23:23:43 +0200

    added widgets to the view

--- src/pages/InvestmentPage.js
+++ src/pages/InvestmentPage.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Jumbotron } from 'reactstrap';
-import { DefaultContainer } from 'layouts/DefaultContainer';
+import { Jumbotron, Container, Row, Col } from 'reactstrap';
 import InvestedStocksTable from 'components/stocks/InvestedStocksTable';
+import BalanceWidget from 'components/widgets/BalanceWidget';
+import StockWorthWidget from 'components/widgets/StockWorthWidget';
+import AccountWidget from 'components/widgets/AccountWidget';
 
 const InvestmentPage = () => (
-  <DefaultContainer>
-    <Jumbotron className="bg-secondary box-shadow pb-0">
-      <h4>Active investments</h4>
-      <InvestedStocksTable />
-    </Jumbotron>
-  </DefaultContainer>
+  <Container>
+    <Row>
+      <Col xs={12} md={4} className="d-flex align-items-stretch">
+        <BalanceWidget />
+      </Col>
+      <Col xs={12} md={4} className="d-flex align-items-stretch">
+        <StockWorthWidget />
+      </Col>
+      <Col xs={12} md={4} className="d-flex align-items-stretch">
+        <AccountWidget />
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={12}>
+        <Jumbotron className="bg-secondary box-shadow pb-0">
+          <h4>Active investments</h4>
+          <InvestedStocksTable />
+        </Jumbotron>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default InvestmentPage;
