3f0833224a32a6b8ae1464e40d81372324e5b240
H4MSK1 <alburhan97@gmail.com>
2019-10-25 23:23:29 +0200

    cleaned up the mess

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import { Jumbotron, Row, Col, Button, Container } from 'reactstrap';
-import { useAlert } from 'react-alert';
+import { Jumbotron, Row, Col, Container } from 'reactstrap';
 import { onlyAuth } from 'api/utils';
-import { addBalance } from 'actions/user';
 import { getStateWithDispatcher } from 'providers/StateManagerProvider';
 import MarketStocksTable from 'components/stocks/MarketStocksTable';
-import AddBalanceDialog from 'components/AddBalanceDialog';
 import StockChart from 'components/stocks/StockChart';
+import BalanceWidget from 'components/widgets/BalanceWidget';
+import StockWorthWidget from 'components/widgets/StockWorthWidget';
 
 const IndexPage = () => {
-  const alert = useAlert();
-  const { state, dispatch } = getStateWithDispatcher();
-  const [balanceDialog, setBalanceDialog] = React.useState(false);
-  const onSubmitAddBalance = async balance => {
-    try {
-      await addBalance(dispatch, balance);
-      alert.success(`${balance} have been added to your balance`);
-    } catch (err) {
-      alert.error(err);
-    }
-  };
-
+  const { state } = getStateWithDispatcher();
   return (
     <>
       <Container>
         <Row>
-          <Col xs={12} md={onlyAuth(() => 8)}>
+          <Col xs={12} lg={onlyAuth(() => 8)}>
             <Jumbotron className="bg-secondary box-shadow pb-0">
               <h4>Market</h4>
               <MarketStocksTable />
@@ -33,50 +21,18 @@ const IndexPage = () => {
           </Col>
 
           {onlyAuth(() => (
-            <Col xs={12} md={4}>
-              <Jumbotron className="bg-secondary box-shadow text-primary">
-                <h5>
-                  <b>Balance available</b>
-                </h5>
-                <h6>
-                  <b>
-                    {state.userBalance} {state.currencySymbol}
-                  </b>
-                </h6>
-                <Button
-                  color="success"
-                  onClick={() => setBalanceDialog(true)}
-                  size="sm"
-                >
-                  Add balance
-                </Button>
-              </Jumbotron>
-              <Jumbotron className="bg-secondary box-shadow text-primary">
-                <h5>
-                  <b>Stock worth</b>
-                </h5>
-                <h6 className="mb-0">
-                  <b>
-                    {state.userStocksValue} {state.currencySymbol}
-                  </b>
-                </h6>
-              </Jumbotron>
+            <Col xs={12} lg={4}>
+              <BalanceWidget />
+              <StockWorthWidget />
             </Col>
           ))}
         </Row>
 
-        {balanceDialog && (
-          <AddBalanceDialog
-            handleSubmit={onSubmitAddBalance}
-            onClosed={() => setBalanceDialog(false)}
-          />
-        )}
-
         <Row>
           {state.marketStocks.map((stock, index) => (
-            <Col xs={12} md={6} key={index}>
+            <Col xs={12} lg={6} key={index}>
               <Jumbotron className="bg-secondary box-shadow">
-                <h4 className="mb-0">{stock.name}</h4>
+                <h4>{stock.name}</h4>
                 <StockChart stock={stock} />
               </Jumbotron>
             </Col>
